897ac10bbe0bf90f013aa1dffe7bad00420a4cb0
JMarcu <james.marcu@yahoo.com>
2021-04-15 21:08:03 -0700

    Tomcat Catalina

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,7 @@ import models.ServerMessage.MessageType;
 import models.ServerMessage.PlayerPropertiesMessageBody;
  
 import org.apache.catalina.WebResourceRoot;
-// import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
@@ -49,22 +49,22 @@ public class App {
 
         tomcat.setPort(Integer.valueOf(webPort));
 
-        // StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        // System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
         // Declare an alternative location for your "WEB-INF/classes" dir
         // Servlet 3.0 annotation will work
-        // File additionWebInfClasses = new File("target/classes");
-        // WebResourceRoot resources = new StandardRoot(ctx);
-        // resources.addPreResources(
-        //     new DirResourceSet(
-        //         resources, 
-        //         "/WEB-INF/classes",
-        //         additionWebInfClasses.getAbsolutePath(),
-        //         "/"
-        //     )
-        // );
-        // ctx.setResources(resources);
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+            new DirResourceSet(
+                resources, 
+                "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(),
+                "/"
+            )
+        );
+        ctx.setResources(resources);
 
         tomcat.start();
         tomcat.getServer().await();
