e96bc0760e6579e6a387472112873da1f7259c18
JMarcu <james.marcu@yahoo.com>
2021-04-17 10:45:03 -0700

    Testing.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,11 @@
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -6,6 +13,7 @@ import models.Client;
 import models.ClientHandler;
 import models.Lobby;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.Engine;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardContext;
@@ -29,15 +37,25 @@ public class App {
         if(webPort == null || webPort.isEmpty()) {
             webPort = "4210";
         }
+        tomcat.setPort(Integer.parseInt(webPort));
         System.out.println("Receiving webport value " + webPort);
 
-        Connector connector = new Connector(new Http11Nio2Protocol());
-        connector.setDomain("0.0.0.0");
-        connector.setPort(Integer.valueOf(webPort));
-        tomcat.setConnector(connector);
-        connector.getService().setContainer(new StandardEngine());
-        connector.getService().getContainer().setDefaultHost("cs4b-tic-tac-toe-lobby-service.herokuapp.com");
-        
+        // Connector connector = new Connector(new Http11Nio2Protocol());
+        // connector.setDomain("0.0.0.0");
+        // connector.setPort(Integer.valueOf(webPort));
+        // connector.
+        // tomcat.setConnector(connector);
+
+        // StandardContext ctx = (StandardContext) tomcat.addContext(
+        //     "cs4b-tic-tac-toe-lobby-service.herokuapp.com", 
+        //     contextPath, 
+        //     contextName, 
+        //     dir
+        // )
+        // Engine engine = new StandardEngine();
+        // connector.getService().setContainer();
+        // connector.getService().getContainer().setDefaultHost("cs4b-tic-tac-toe-lobby-service.herokuapp.com");
+                
         // StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         // System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
@@ -55,6 +73,9 @@ public class App {
         // );
         // ctx.setResources(resources);
 
+        String webappDirLocation = setupWebapp();
+        // File thisJar = new File(App.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
 
         tomcat.start();
         tomcat.getServer().await();
@@ -122,6 +143,61 @@ public class App {
             //     //     System.out.println();
             //     // }
             // }
+    }
+
+    private static String setupWebapp() throws Exception
+    {
+        File thisJar = new File(App.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        System.out.println("thisJar: " + thisJar);
+        String target = "tempwebapp";
+        File targetFolder = new File(target + File.separator + "WEB-INF" + File.separator +"lib");
+        System.out.println("targetFolder: " + targetFolder);
+        targetFolder.mkdirs();
+        copyFileUsingChannel(thisJar, new File(targetFolder.getPath() + File.separator + "lobby-service-1.0.jar"));
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        try {
+            stream = App.class.getResourceAsStream("index.html");
+            if(stream == null) {
+                throw new Exception("Cannot get resource \"index.html\" from Jar file.");
+            }
+ 
+            int readBytes;
+            byte[] buffer = new byte[4096];
+ 
+            resStreamOut = new FileOutputStream(target + File.separator + "index.html");
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+ 
+ 
+        }
+        return target;
+    }
+ 
+    private static void copyFileUsingChannel(File source, File dest) throws IOException {
+        System.out.println(source);
+        System.out.println(dest);
+        FileInputStream sourceStream = null;
+        FileChannel sourceChannel = null;
+        FileChannel destChannel = null;
+        try {
+            sourceStream = (new FileInputStream(source));
+            System.out.println("sourceStream: " + sourceStream);
+            sourceChannel = sourceStream.getChannel();
+            System.out.println("sourceChannel: " + sourceChannel);
+            destChannel = new FileOutputStream(dest).getChannel();
+            System.out.println("destChannel: " + destChannel);
+            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+        }
+        sourceStream.close();
+        destChannel.close();
     }
 
     public static int randomLobbySearch(ArrayList<Lobby> lobbyList){
