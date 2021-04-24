279be9b5d2046fd27966225229a0e1af44eeeb79
Sarah Whitelaw <sarah.rose.whitelaw@gmail.com>
2021-04-23 17:51:46 -0700

    updated Game model

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -17,13 +17,52 @@ public class Game {
     private Integer winner;
     private Integer gameId;
 
-    public Game(List<Integer> people){ // probably change what it takes in
+    public Game(List<Integer> people, Integer gameNumber){ // probably change what it takes in
         start = System.currentTimeMillis();
         end = -1;
         players = people;
         winner = -1;
-        gameId = -1; // CHANGE //////////////////////////
+        gameId = gameNumber;
         moves = new ArrayList<Pair<Integer,Integer>>();
     }
 
+    public void addPlayer(Integer player){
+        players.add(player);
+    }
+
+    public void setEnd(){
+        end = System.currentTimeMillis();
+    }
+
+    public void addMove(Pair<Integer,Integer> move){
+        moves.add(move);
+    }
+
+    public void setWinner(Integer winnerId){
+        winner = winnerId;
+    }
+
+    public Integer getWinner(){
+        return winner;
+    }
+
+    public Integer getGameId(){
+        return gameId;
+    }
+
+    public long getStart(){
+        return start;
+    }
+
+    public long getEnd(){
+        return end;
+    }
+
+    public List<Pair<Integer,Integer>> getMoves(){
+        return moves;
+    }
+
+    public List<Integer> getPlayers(){
+        return players; // first player, second player, viewers
+    }
 }
