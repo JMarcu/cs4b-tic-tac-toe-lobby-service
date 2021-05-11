cb6c2169e0d6426d7e4823a7450cdb7f0999b6e3
Sarah Whitelaw <sarah.rose.whitelaw@gmail.com>
2021-05-10 21:07:28 -0700

    added psuedocode

--- src/main/java/interfaces/PlayerDatabaseInterface.java
+++ src/main/java/interfaces/PlayerDatabaseInterface.java
@@ -4,25 +4,32 @@ import models.Player;
 
 public class PlayerDatabaseInterface {
 
-    public Integer validatePassword(String playerId, String password){
-        Integer hash = 0; // make it not 0 later
+    public Boolean validatePassword(String playerId, String password){
+        Boolean isValid = true; // make it not 0 later
         //TODO
-        return hash;
+        return isValid;
     }
     
     public Boolean setPassword(String playerId, String password){
         Boolean isSet = true; // change
         //TODO
+        //use sha1 or other hash generator to make hash
+        //only allow to set password when making account
+        //INSERT INTO player(playerNum, passwordHash)
+        //Values(playerId, hash);
         return isSet;
     }
 
     public String getToken(String playerId){
         //TODO
+        //SELECT token FROM tokens WHERE playerNum = playerId;
         return "TODO";
     }
 
     public void setToken(String playerID, String token){
         //TODO
+        //INSERT INTO RefreshTokens
+        //throw an error if it doesn't work
     }
 
     public void deleteToken(String playerId){
