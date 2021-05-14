506d8704411768314189fc7674766a9f4a9ea229
Sarah Whitelaw <sarah.rose.whitelaw@gmail.com>
2021-05-13 23:47:57 -0700

    commit

--- src/main/java/interfaces/PlayerDatabaseInterface.java
+++ src/main/java/interfaces/PlayerDatabaseInterface.java
@@ -221,11 +221,13 @@ public class PlayerDatabaseInterface {
         //TODO
         try{
             Connection conn = getConn();
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO Players(playerId, markerShape, markerColor, username) VALUES (?,?,?,?)");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO Players(playerId, markerShape, markerColor, username) VALUES (?,?,?,?) ON CONFLICT (playerId) DO UPDATE SET markerShape = ?, markerColor = ?");
             stmt.setString(1, player.getUuid().toString());
             stmt.setString(2, player.getShape().toString());
             stmt.setString(3, player.getColor().toString());
             stmt.setString(4, player.getName());
+            stmt.setString(5, player.getShape().toString());
+            stmt.setString(6, player.getColor().toString());
             stmt.executeUpdate();
             //close everything
             stmt.close();
