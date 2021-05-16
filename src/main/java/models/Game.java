c67f3d4ed8ef1968f791ae11e250527fac467e88
Sarah Whitelaw <sarah.rose.whitelaw@gmail.com>
2021-05-16 01:56:34 -0700

    updated GameDatabaseInterface and Game model

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.ArrayList;
 import javafx.util.Pair;
 
@@ -8,23 +9,43 @@ public class Game {
     
     private long start;
     private long end;
-    private List<String> spectators; // takes PlayerIds instead of Players, so Integer
+    private List<UUID> spectators; // takes PlayerIds instead of Players, so Integer
     private List<Pair<Integer,Integer>> moves;
-    private String winner;
+    private UUID winner;
     private Integer gameId;
-    private Pair<String,String> players;
+    private Pair<UUID,UUID> players;
 
-    public Game(Pair<String,String> people, Integer gameNumber){ // probably change what it takes in
+    public Game(){ // probably change what it takes in
+    }
+
+    public Game(Pair<UUID,UUID> people, Integer gameNumber){ // probably change what it takes in
         start = System.currentTimeMillis();
         end = -1;
         players = people;
-        winner = "";
+        winner = UUID.fromString("");
         gameId = gameNumber;
         moves = new ArrayList<Pair<Integer,Integer>>();
-        spectators = new ArrayList<String>();
+        spectators = new ArrayList<UUID>();
     }
 
-    public void addSpecator(String spectator){
+    public Game(long gameStart, long gameEnd, List<UUID> gameSpectators, List<Pair<Integer,Integer>> gameMoves, UUID gameWinner, Integer gameNumber, Pair<UUID,UUID> people){ // probably change what it takes in
+        start = gameStart;
+        end = gameEnd;
+        players = people;
+        winner = gameWinner;
+        gameId = gameNumber;
+        moves = gameMoves;
+        spectators = gameSpectators;
+    }
+    public void setStart(long gameStart){
+        start = gameStart;
+    }
+
+    public void setGameId(int gameNum)
+    {
+        gameId = gameNum;
+    }
+    public void addSpecator(UUID spectator){
         spectators.add(spectator);
     }
 
@@ -32,15 +53,27 @@ public class Game {
         end = System.currentTimeMillis();
     }
 
+    public void setEndManually(long gameEnd){
+        end = gameEnd;
+    }
+
+    public void setAllSpectators(List<UUID> gameSpectators){
+        spectators = gameSpectators;
+    }
+
     public void addMove(Pair<Integer,Integer> move){
         moves.add(move);
     }
 
-    public void setWinner(String winnerId){
+    public void setAllMoves(List<Pair<Integer,Integer>> allMoves){
+        moves = allMoves;
+    }
+
+    public void setWinner(UUID winnerId){
         winner = winnerId;
     }
 
-    public String getWinner(){
+    public UUID getWinner(){
         return winner;
     }
 
@@ -60,11 +93,15 @@ public class Game {
         return moves;
     }
 
-    public Pair<String,String> getPlayers(){
+    public Pair<UUID,UUID> getPlayers(){
         return players; // first player, second player
     }
 
-    public List<String> getSpectators(){
+    public void setPlayers(Pair<UUID,UUID> people){
+        players = people;
+    }
+
+    public List<UUID> getSpectators(){
         return spectators;
     }
 }
